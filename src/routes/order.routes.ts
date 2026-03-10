4eab89de1e5579b5fedbfa6135ef0e6d90650f63
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2026-03-10 16:29:57 -0400

    update shipping info updated

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -1,9 +1,10 @@
 import passport from "../auth/passport";
-import { createorder,getorderhistory } from "../controllers/order.controller";
+import { createorder,getorderhistory, updateshippinginfo } from "../controllers/order.controller";
 import { handleAsyncErr } from "../utils/handleAsyncErr";
 import { Router } from "express";
 
 const router = Router();
 router.post('/',passport.authenticate('jwt', { session: false }), handleAsyncErr(createorder));
 router.get('/orderhistory', passport.authenticate('jwt', { session: false }), handleAsyncErr(getorderhistory));
+router.post('/edit-shipping-info', passport.authenticate('jwt', { session: false }), handleAsyncErr(updateshippinginfo));
 export default router;
