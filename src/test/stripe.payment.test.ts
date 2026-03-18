25e26e328bd9717f241396d432c841d7633eb7d6
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2026-03-18 14:48:19 -0400

    husky updated

--- src/test/stripe.payment.test.ts
+++ src/test/stripe.payment.test.ts
@@ -1,43 +1,43 @@
-// import request from 'supertest';
-// import app from '../server';
-// import dotenv from 'dotenv';
+import request from 'supertest';
+import app from '../server';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-// let accesstoken: string
+let accesstoken: string;
 
-// beforeAll(async () => {
-//     const response = await request(app)
-//         .post('/api/auth/token')
-//         .set('Cookie', `refreshToken=${process.env.TEST_REFRESH_TOKEN}`)
+beforeAll(async () => {
+  const response = await request(app)
+    .post('/api/auth/token')
+    .set('Cookie', `refreshToken=${process.env.TEST_REFRESH_TOKEN}`);
 
-//     accesstoken = response.headers['set-cookie'][0].split(';')[0].split('=')[1];
-// })
+  accesstoken = response.headers['set-cookie'][0].split(';')[0].split('=')[1];
+});
 
-// describe('stripe payment routes', () => {
-//     it('should create a stripe payment intent', async () => {
-//         const response = await request(app)
-//         .post('/api/payment/stripe/create-payment-intent')
-//         .set('Cookie', `accessToken=${accesstoken}`)
-//         .send({
-//             total_price: "136,000",
-//             currency: "cad",
-//             order_id: 8
-//         })
-//         expect(response.status).toBe(200);
-//         expect(response.body).toHaveProperty('client_secret');
-//         expect(response.body).toHaveProperty('paymentIntent_id');
-//         console.log(response.body.paymentIntent_id);
-//         console.log(response.body.client_secret);
-//     })
-// })
+describe('stripe payment routes', () => {
+  it('should create a stripe payment intent', async () => {
+    const response = await request(app)
+      .post('/api/payment/stripe/create-payment-intent')
+      .set('Cookie', `accessToken=${accesstoken}`)
+      .send({
+        total_price: '136,000',
+        currency: 'cad',
+        order_id: 8,
+      });
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('client_secret');
+    expect(response.body).toHaveProperty('paymentIntent_id');
+    console.log(response.body.paymentIntent_id);
+    console.log(response.body.client_secret);
+  });
+});
 
-// describe('stripe payment verification route', () => {
-//     it('should verify a stripe payment', async () => {
-//         const response = await request(app)
-//         .get(`/api/payment/stripe/verify-payment/${process.env.PAYMENTINTENTID}`)
-//         .set('Cookie', `accessToken=${accesstoken}`)
-//         expect(response.status).toBe(400);
-//         expect(response.body).toEqual({ message: "Payment not successful" })
-//     })
-// })
+describe('stripe payment verification route', () => {
+  it('should verify a stripe payment', async () => {
+    const response = await request(app)
+      .get(`/api/payment/stripe/verify-payment/${process.env.PAYMENTINTENTID}`)
+      .set('Cookie', `accessToken=${accesstoken}`);
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({ message: 'Payment not successful' });
+  });
+});
