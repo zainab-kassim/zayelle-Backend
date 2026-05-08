f1be4800f61e28bb1cf1e357f99a1882641b4d08
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2026-05-08 15:49:04 -0400

    fix:pass currency to product return object

--- src/addProduct.ts
+++ src/addProduct.ts
@@ -2,21 +2,21 @@ import { supabase } from './config/db';
 import { generateUniqueSlug } from './middleware/generateSlug';
 
 export async function addProduct() {
-  const name = 'Zayelle Gradient Fringe';
+  const name = 'Zayelle floreal-red';
   const slug = await generateUniqueSlug(name);
 
   // Implementation for adding a product
   await supabase.from('products').insert([
     {
-      collectionid: 1,
+      collectionid: 11,
       name: name,
       slug: slug,
-      description: 'This is a luxurious weave from Zayelle orange fabric',
-      price: '38,000',
+      description: 'This is a red piece from the zayelle floreal collection',
+      price: '120',
       size: ['S', 'M', 'L'],
       quantity: 6,
       image: [
-        'https://oqk3pkp15w.ufs.sh/f/7063dead-6f6b-4c1e-b7e4-5e9d68a5050f-eqkwve.jpg',
+        'https://oqk3pkp15w.ufs.sh/f/H3vgRA928TvFIegFnMvqydlKEzCBswxIZt7GTo168RJY9Lek',
         'https://oqk3pkp15w.ufs.sh/f/0c9a9627-b2d1-445a-be1d-4a2233c7b9b0-57kj6q.jpg',
       ],
     },
