d5ebe148b339f37d393e527cf665612d89436465
Zainab Kassim Temitope <kassimzainab11@gmail.com>
2026-06-04 15:18:25 -0400

    fix:cors options updated

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,9 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const corsMiddleware = cors({
-  origin: [process.env.FRONTEND_URL, process.env.LOCAL_URL].filter(
-    Boolean,
-  ) as string[], // Allow requests from this origin
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // Allow requests from this origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true, // Allow cookies to be sent
   allowedHeaders: ['Content-Type', 'Authorization', 'x-currency'], // Allow specific headers
